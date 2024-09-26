3cbdd1cae8d5dd508df3dcb817a36b639e870165
epashiq <ashiqashi4715@gmail.com>
2024-09-26 14:20:17 +0530

    update ui

--- lib/view/pages/kapta_page.dart
+++ lib/view/pages/kapta_page.dart
@@ -15,11 +15,12 @@ class _KaptaPageState extends State<KaptaPage> {
       appBar: AppBar(
         backgroundColor: const Color(0XFF31353A5D),
         leading: IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.arrow_back_ios,
-              color: Colors.white,
-            )),
+          onPressed: () {},
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
+          ),
+        ),
         title: const Text(
           'Адрес',
           style: TextStyle(
@@ -31,54 +32,115 @@ class _KaptaPageState extends State<KaptaPage> {
       ),
       body: Column(
         children: [
-          SizedBox(
-            height: 100,
-          ),
-          Center(
+          const SizedBox(height: 200),
+          const Center(
             child: Text(
               'Выберите тип вывода',
               style: TextStyle(
-                  fontSize: 21,
-                  fontWeight: FontWeight.w700,
-                  color: Colors.white),
+                fontSize: 21,
+                fontWeight: FontWeight.w700,
+                color: Colors.white,
+              ),
             ),
+          ),
+          const SizedBox(height: 20),
+          Container(
+            height: 128,
+            width: 343,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(24),
+              color: const Color(0XFF2F3C50),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/Group 237703 (1).png',
+                  height: 80, // Adjust the image size as needed
+                ),
+                const SizedBox(width: 10),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Банковский счет',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      'К сожалению, мы не знаем, куда\nотправить ваши деньги.',
+                      style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                        color: Color(0XFFCDCDCD),
+                      ),
+                    ),
+                  ],
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon:
+                      const Icon(Icons.arrow_forward_ios, color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20,
           ),
           Container(
             height: 128,
             width: 343,
             decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(24),
-                color: Color(0XFF2F3C50)),
+              borderRadius: BorderRadius.circular(24),
+              color: const Color(0XFF2F3C50),
+            ),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Image.asset('assets/images/Group 237703 (1).png'),
-                SizedBox(
-                  height: 10,
+                Image.asset(
+                  'assets/images/Group 237703.png',
+                  height: 80,
                 ),
+                const SizedBox(width: 10),
                 Column(
-                  children: [
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
                     Text(
                       'Банковский счет',
                       style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.white),
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                      ),
                     ),
+                    SizedBox(height: 5),
                     Text(
                       'К сожалению, мы не знаем, куда\nотправить ваши деньги.',
                       style: TextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.w400,
-                          color: Color(0XFFCDCDCD)),
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                        color: Color(0XFFCDCDCD),
+                      ),
                     ),
                   ],
                 ),
                 IconButton(
-                    onPressed: () {}, icon: Icon(Icons.arrow_forward_ios))
+                  onPressed: () {},
+                  icon:
+                      const Icon(Icons.arrow_forward_ios, color: Colors.white),
+                ),
               ],
             ),
-          )
+          ),
         ],
       ),
     );
